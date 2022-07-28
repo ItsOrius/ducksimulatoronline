60e225d19b1f909826f44719042a52e413e23593
ItsOrius <thegameradmin@icloud.com>
2022-07-28 16:23:57 -0400

    rookie chatter now needs rank 15

--- routes/password.js
+++ routes/password.js
@@ -16,4 +16,37 @@ router.get("/:password", async (req, res) => {
   }
 });
 
-module.exports = { router };
+function claimBounty(password, claimerId, res) {
+  const secrets = require("../secrets.json");
+  if (!secrets[password]) {
+    throw new Error("Invalid password.");
+  }
+  const client = require("../index.js")
+  const config = require("../config.json")
+  client.guilds.fetch(config.guildId).then(async guild => {
+    guild.members.fetch(`${claimerId}`).then(member => {
+      const db = require("../db.json");
+      const hunter = db.users[claimerId];
+      const bounty = db.users[secrets[password]];
+
+    }).catch(e => {
+      throw new Error("User not found.");
+    })
+  }).catch(e => {
+    if (res) {
+      res.status(500).json({ error: "Failed to reach Discord server." });
+    } else {
+      throw new Error("Failed to reach Discord server.");
+    }
+  });
+}
+
+router.post("/:password/claim", (req, res) => {
+  try {
+    claimBounty(req.params.password, req.body.claimerId, res);
+  } catch(e) {
+    res.status(404).json({ error: e.message });
+  }
+});
+
+module.exports = { router, claimBounty }
