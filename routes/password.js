93d6a8d8349655282c10a457b3cdcdbffb187aee
ItsOrius <thegameradmin@icloud.com>
2022-07-28 16:54:14 -0400

    finished password/:password/claim route

--- routes/password.js
+++ routes/password.js
@@ -5,11 +5,11 @@ router.get("/:password", async (req, res) => {
   const { password } = req.params;
   if (secrets[password]) {
     require("./profile.js").getProfile(secrets[password]).then(profile => {
-      if (profile) {
-        res.status(200).json({ id: secrets[password], profile });
-      } else {
+      if (!profile) {
         res.status(500).json({ error: "Failed to get user profile!" });
+        return;
       }
+      res.status(200).json({ id: secrets[password], profile });
     });
   } else {
     res.status(404).json({ error: "Invalid password." });
@@ -18,15 +18,22 @@ router.get("/:password", async (req, res) => {
 
 function claimBounty(password, claimerId, res) {
   const secrets = require("../secrets.json");
+  const db = require("../db.json");
   if (!secrets[password]) {
     throw new Error("Invalid password.");
+  }
+  if (db[secrets[password]].claimer) {
+    throw new Error("This password has already been claimed.");
+  }
+  if (secrets[password] === claimerId) {
+    throw new Error("You cannot claim your own bounty.");
   }
   const client = require("../index.js")
   const config = require("../config.json")
   return client.guilds.fetch(config.guildId).then(async guild => {
     return guild.members.fetch(`${claimerId}`).then(member => {
-      const db = require("../db.json");
       db.users[secrets[password]].config.claimer = claimerId;
+      db.users[claimerId].config.claims++;
       fs.writeFileSync("./db.json", JSON.stringify(db));
       if (res) {
         res.status(200).json({ message: "Successfully claimed!" });
@@ -38,9 +45,9 @@ function claimBounty(password, claimerId, res) {
   }).catch(e => {
     if (res) {
       res.status(500).json({ error: "Failed to reach Discord server." });
-    } else {
-      throw new Error("Failed to reach Discord server.");
+      return;
     }
+    throw new Error("Failed to reach Discord server.");
   });
 }
 
