c0e13d329939b984529f369f7ee8fe19c70d1aa8
Orius <32719997+ItsOrius@users.noreply.github.com>
2022-08-02 01:54:25 +0000

    hide password on join until further testing can happen

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -11,7 +11,7 @@ function execute(client, member) {
   // direct message member with password
   const channel = member.user.dmChannel || member.user.createDM();
   const password = passwordCommand.generatePassword(member.id);
-  channel.send({ content: `**Welcome to the official Duck Simulator server!**\nYour personal password is ||${password}||. **DO NOT SHARE THIS WITH ANYONE ELSE!**` });
+  //channel.send({ content: `**Welcome to the official Duck Simulator server!**\nYour personal password is ||${password}||. **DO NOT SHARE THIS WITH ANYONE ELSE!**` });
 }
 
 module.exports = { name, execute };
