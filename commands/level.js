4c22e460dc24b815e8356f596d941c7aeef54a5f
ItsOrius <thegameradmin@icloud.com>
2022-08-17 11:16:34 -0400

    added next reward field

--- commands/level.js
+++ commands/level.js
@@ -8,6 +8,7 @@ const data = new SlashCommandBuilder()
 
 async function execute(client, interaction) {
   const manager = require('../LeaderboardManager');
+  const { levelRoles } = require('../config.json');
   let user = interaction.options.getUser('user');
   if (!user) {
     user = interaction.user;
@@ -20,12 +21,21 @@ async function execute(client, interaction) {
       nextLevelXp: manager.getNeededXp(1)
     };
   }
+  const nextReward = { level: -1, id: -1 };
+  for (let i = 0; i < Object.keys(levelRoles); i++) {
+    if (profile.level < Object.keys(levelRoles)[i]) {
+      nextReward.level = Object.keys(levelRoles)[i]
+      nextReward.id = Object.values(levelRoles)[i];
+      break;
+    }
+  }
   const embed = new Discord.MessageEmbed()
     .setTitle(`${user.tag}'s Level`)
     .addFields([
       { name: "Level", value: `${profile.level}`, inline: true },
       { name: "Total XP", value: `${profile.xp}`, inline: true },
-      { name: "Progress", value: `${profile.xp - manager.getNeededXp(profile.level)}/${profile.nextLevelXp - manager.getNeededXp(profile.level)}` }
+      { name: "Progress", value: `${profile.xp - manager.getNeededXp(profile.level)}/${profile.nextLevelXp - manager.getNeededXp(profile.level)}` },
+      { name: "Next Reward", value: `Level ${nextReward.level} (<@&${nextReward.id}>)`, inline: true }
     ])
     .setColor(interaction.member.displayHexColor)
   await interaction.reply({ embeds: [embed], ephemeral: true });
