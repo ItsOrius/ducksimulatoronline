a160ea2e5a368e1d3324151d4ff32a9bed1623ca
ItsOrius <thegameradmin@icloud.com>
2022-08-20 13:37:31 -0400

    error messages are now better formatted

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ client.on('interactionCreate', async interaction => {
     await command.execute(client, interaction);
   } catch (error) {
     //console.error(error);
-    await interaction.reply({ content: `**There was an error while executing this command!**\nClick the black box below to see the error.\n||${error}||`, ephemeral: true });
+    await interaction.reply({ content: `**There was an error while executing this command!**\`\`\`${error}\`\`\``, ephemeral: true });
   }
 });
 
