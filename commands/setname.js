0ae3f8e46cbf3ce4b148d17fd50e0365829c8350
ItsOrius <thegameradmin@icloud.com>
2022-08-20 15:35:01 -0400

    created setname command

---
+++ commands/setname.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder()
+	.setName('setname')
+	.setDescription("If you've ever been Verified, you can set the name that appears in Duck Simulator 3's credits!")
+  .addStringOption(option => option.setName('name').setDescription('The name to use.').setRequired(true))
+
+/**
+ * 
+ * @param {Discord.Client} client 
+ * @param {Discord.Interaction} interaction 
+ */
+async function execute(client, interaction) {
+  const db = require('../db.json');
+  const premiumLevel = require("../events/guildMemberUpdate.js").getBestPremiumStatus(interaction.member);
+  if (premiumLevel < 4) {
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Error')
+      .setDescription('You need to have been Verified to use this command!')
+      .setColor("RED");
+    return interaction.reply({ embeds: [embed], ephemeral: true });
+  }
+  const name = interaction.options.getString('name');
+  db[interaction.user.id].name = name;
+  fs.writeFileSync('./db.json', JSON.stringify(db));
+  const embed = new Discord.MessageEmbed()
+    .setTitle('Success')
+    .setDescription(`Your name has been set to \`\`${name}\`\`!`)
+    .setColor("GREEN");
+  return interaction.reply({ embeds: [embed], ephemeral: true });
+}
+
+module.exports = { data, execute };
--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,30 +2,43 @@ const Discord = require('discord.js');
 const fs = require("fs");
 const name = 'guildMemberUpdate';
 
+function getBestPremiumStatus(member) {
+  const db = require("../db.json");
+  const { premiumRoles } = require("../config.json");
+  const manager = require("../LeaderboardManager.js");
+  if (!db[member.id]) {
+    db[member.id] = new manager.UserProfile(member.user.username, member.user.discriminator, member.user.avatarURL(), 0, 0, {});
+  }
+  const premiumStatus = db[member.id].highestPremium || 0;
+  Object.entries(premiumRoles).forEach(role => {
+    if (member.roles.cache.has(role[0])) {
+      if (premiumStatus < parseFloat(role[1])) {
+        db[member.id].highestPremium = parseFloat(role[1]);
+      }
+    }
+  });
+  if (!db[member.id].highestPremium) {
+    db[member.id].highestPremium = 0;
+  }
+  return db[member.id].highestPremium;
+}
+
 /**
  * @param {Discord.Client} client 
  * @param {Discord.PartialGuildMember} oldMember 
  * @param {Discord.GuildMember} newMember 
  */
 function execute(client, oldMember, newMember) {
+  const premiumStatus = getBestPremiumStatus(newMember);
   const db = require("../db.json");
-  const { premiumRoles } = require("../config.json");
-  const manager = require("../LeaderboardManager.js");
-  if (!db[newMember.id]) {
-    db[newMember.id] = new manager.UserProfile(newMember.user.username, newMember.user.discriminator, newMember.user.avatarURL(), 0, 0, {});
+  if (!db[member.id]) {
+    db[member.id] = new manager.UserProfile(member.user.username, member.user.discriminator, member.user.avatarURL(), 0, 0, {});
   }
-  const premiumStatus = db[newMember.id].highestPremium || 0;
-  Object.entries(premiumRoles).forEach(role => {
-    if (newMember.roles.cache.has(role[0])) {
-      if (premiumStatus < parseFloat(role[1])) {
-        db[newMember.id].highestPremium = parseFloat(role[1]);
-      }
-    }
-  });
-  if (!db[newMember.id].highestPremium) {
-    db[newMember.id].highestPremium = 0;
+  if (premiumStatus <= db[member.id].highestPremium) {
+    return;
   }
+  db[newMember.id].premiumStatus = premiumStatus;
   fs.writeFileSync("./db.json", JSON.stringify(db));
 }
 
-module.exports = { name, execute };
+module.exports = { name, execute, getBestPremiumStatus };
