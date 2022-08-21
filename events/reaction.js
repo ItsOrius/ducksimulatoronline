b1aee3ba4ba0e0a332feb66fca5634c9c9d534fb
ItsOrius <thegameradmin@icloud.com>
2022-08-21 01:19:04 -0400

    removed awaitReactions call

--- events/reaction.js
+++ events/reaction.js
@@ -3,7 +3,7 @@ const names = ["messageReactionAdd", "messageReactionRemove"];
 
 /**
  * @param {Discord.Client} client
- * @param {Discord.PartialMessageReaction} reaction
+ * @param {Discord.MessageReaction} reaction
  * @param {Discord.User} user
  */
 async function execute(client, reaction, user) {
@@ -12,19 +12,17 @@ async function execute(client, reaction, user) {
   if (reaction.message.channelId != config.suggestionsChannel) return;
   if (user.id == client.user.id) return;
   const message = reaction.message;
-  message.awaitReactions().then(collected => {
-    const reactions = collected.array();
-    const yesReaction = reactions.find(reaction => reaction.emoji.name == "⬆️");
-    const noReaction = reactions.find(reaction => reaction.emoji.name == "⬇️");
-    const ratio = yesReaction.count / (yesReaction.count + noReaction.count);
-    if (ratio > 0.5) {
-      reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "GREEN", image: message.embeds[0].image.url }] });
-    } else if (ratio < 0.5) {
-      reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "RED", image: message.embeds[0].image.url }] });
-    } else {
-      reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: config.botColor, image: message.embeds[0].image.url }] });
-    }
-  });
+  const reactions = message.reactions.cache;
+  const yesReaction = reactions.find(reaction => reaction.emoji.name == "⬆️");
+  const noReaction = reactions.find(reaction => reaction.emoji.name == "⬇️");
+  const ratio = yesReaction.count / (yesReaction.count + noReaction.count);
+  if (ratio > 0.5) {
+    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "GREEN", image: message.embeds[0].image.url }] });
+  } else if (ratio < 0.5) {
+    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "RED", image: message.embeds[0].image.url }] });
+  } else {
+    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: config.botColor, image: message.embeds[0].image.url }] });
+  }
 }
 
 module.exports = { names, execute };
