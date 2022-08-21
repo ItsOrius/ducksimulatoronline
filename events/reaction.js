075ebeab90469a7f105441c325f92e119e7fea72
ItsOrius <thegameradmin@icloud.com>
2022-08-21 01:23:26 -0400

    made nearly all embed parts optional

--- events/reaction.js
+++ events/reaction.js
@@ -12,17 +12,21 @@ async function execute(client, reaction, user) {
   if (reaction.message.channelId != config.suggestionsChannel) return;
   if (user.id == client.user.id) return;
   const message = reaction.message;
-  const reactions = message.reactions.cache;
-  const yesReaction = reactions.find(reaction => reaction.emoji.name == "⬆️");
-  const noReaction = reactions.find(reaction => reaction.emoji.name == "⬇️");
+  const yesReaction = message.reactions.cache.find(reaction => reaction.emoji.name == "⬆️");
+  const noReaction = message.reactions.cache.find(reaction => reaction.emoji.name == "⬇️");
   const ratio = yesReaction.count / (yesReaction.count + noReaction.count);
+  const embed = new Discord.MessageEmbed().setTitle("Suggestion");
+  if (message.embeds[0].description) embed.setDescription(message.embeds[0].description);
+  if (message.embeds[0].footer) embed.setFooter(message.embeds[0].footer);
+  if (message.embeds[0].timestamp) embed.setTimestamp(message.embeds[0].timestamp);
   if (ratio > 0.5) {
-    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "GREEN", image: message.embeds[0].image.url }] });
+    embed.setColor("GREEN");
   } else if (ratio < 0.5) {
-    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: "RED", image: message.embeds[0].image.url }] });
+    embed.setColor("RED");
   } else {
-    reaction.message.edit({ embeds: [{ title: message.embeds[0].title, description: message.embeds[0].description, color: config.botColor, image: message.embeds[0].image.url }] });
+    embed.setColor(config.botColor);
   }
+  reaction.message.edit({ embeds: [embed] });
 }
 
 module.exports = { names, execute };
