245c4b7164e5f27a9471ca845dc519e864a15267
Orius <32719997+ItsOrius@users.noreply.github.com>
2022-09-10 03:42:03 +0000

    fixed join messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,29 +10,33 @@ const config = require("../config.json");
 function execute(client, member) {
   // direct message member with embed
   const embed = new Discord.MessageEmbed()
-    .setTitle("Welcome to the official Duck Simulator Discord server!")
+    .setTitle("Welcome to the Duck Simulator Discord Server!")
     .setThumbnail(client.guilds.cache.get(config.guildId).iconURL())
     .setColor(config.botColor)
-    .setDescription(`**Thank you so much for joining our server!** Here's a few things you can do to get started:`)
+    .setDescription(`**Thank you so much for joining our server!**\nHere's a couple of things that you can do to get started:`)
     .addFields([
       {
         name: "Get some roles!",
-        value: "Use the ``/roles`` command in any channel to get a list of **roles you can earn**!"
+        value: "Use the ``/roles`` command in any channel to get a list of **roles that you're able to earn**!",
+        inline: true
       },
       {
         name: "Link your account!",
-        value: "Use the ``/password`` command to **receive a personal code** to use in Duck Simulator 2!"
+        value: "Use the ``/password`` command to **receive a personal code** to use in Duck Simulator 2!",
+        inline: true
       },
       {
-        name: "Grind for a whole bunch of XP!",
-        value: "Talk in the server to get a **higher position** on our [custom leaderboard](https://ducksimulator.com/leaderboard)!"
+        name: "Grind for XP!",
+        value: "Talk in the server to get rewards and a **higher position** on our [custom leaderboard](https://ducksimulator.com/leaderboard)!",
+        inline: true
       },
       {
         name: "See how fast people can speedrun the game!",
-        value: "Check the ``#⌚speedrun-feed`` channel for the latest **speedrun times**!"
+        value: "Check out ``#⌚speedrun-feed`` for the latest **speedrun times**!",
+        inline: true
       }
     ])
-    .setFooter({ text: "Duck Simulator", iconURL: client.user.avatarURL() })
+    .setFooter({ text: "Sent by " + client.user.username, iconURL: client.user.avatarURL() })
     .setTimestamp();
   member.send({ embeds: [embed] });
 }
