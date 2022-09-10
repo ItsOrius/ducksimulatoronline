495f3c8568404c3fda385d06a9a750cab70ee68d
ItsOrius <thegameradmin@icloud.com>
2022-09-09 23:23:28 -0400

    added join dm message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const names = ['guildMemberAdd'];
 
-const passwordCommand = require('../commands/password.js');
+const config = require("../config.json");
 
 /**
  * @param {Discord.Client} client
@@ -10,8 +10,30 @@ const passwordCommand = require('../commands/password.js');
 function execute(client, member) {
   // direct message member with password
   const channel = member.user.dmChannel || member.user.createDM();
-  const password = passwordCommand.generatePassword(member.id);
-  //channel.send({ content: `**Welcome to the official Duck Simulator server!**\nYour personal password is ||${password}||. **DO NOT SHARE THIS WITH ANYONE ELSE!**` });
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Welcome to the official Duck Simulator Discord server!")
+    .setImage(client.guilds.cache.get(config.guildId).iconURL())
+    .setColor(config.botColor)
+    .setDescription(`**Thank you so much for joining our server!** Here's a couple of things you can do to get started:`)
+    .addFields([
+      {
+        name: "Get Roles",
+        value: "Use the ``/roles`` command in any channel to get a list of **roles you can earn**!"
+      },
+      {
+        name: "Link Account",
+        value: "Send a message in any channel and use the ``/password`` command to **receive a personal code** to use in Duck Simulator 2!"
+      },
+      {
+        name: "Level Up",
+        value: "Talk in the server to get a **higher position** on our [custom leaderboard](https://ducksimulator.com/leaderboard)!"
+      },
+      {
+        name: "Speedrun",
+        value: "Check the ``⌚speedrun-feed`` channel for the latest **speedrun times**!"
+      }
+    ]);
+  channel.send({ embeds: [embed] });
 }
 
 module.exports = { names, execute };
