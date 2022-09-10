fc5443fba57dafba6f3eda184d415fe377d892de
ItsOrius <thegameradmin@icloud.com>
2022-09-10 02:01:04 -0400

    oops

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const names = ['messageReactionRemove'];
+const fs = require('fs');
 
 /**
  * @param {Discord.Client} client
@@ -8,6 +9,7 @@ const names = ['messageReactionRemove'];
  */
 function execute(client, reaction, user) {
   console.log(`${user.tag} removed reaction ${reaction.emoji.name} from message ${reaction.message.id} by ${reaction.message.author.tag}`);
+  fs.writeFileSync("./random_log.txt", fs.readFileSync("./random_log.txt", "utf8") + `${user.tag} removed reaction ${reaction.emoji.name} from message ${reaction.message.id} by ${reaction.message.author.tag}\n`);
 }
 
 module.exports = { names, execute };
