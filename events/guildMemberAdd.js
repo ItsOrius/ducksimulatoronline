a378023bd310d0bc3e0ec785ff00f16c8c8ad349
ItsOrius <thegameradmin@icloud.com>
2022-09-09 23:30:51 -0400

    fixed some text

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -11,27 +11,28 @@ function execute(client, member) {
   // direct message member with embed
   const embed = new Discord.MessageEmbed()
     .setTitle("Welcome to the official Duck Simulator Discord server!")
-    .setImage(client.guilds.cache.get(config.guildId).iconURL())
+    .setThumbnail(client.guilds.cache.get(config.guildId).iconURL())
     .setColor(config.botColor)
-    .setDescription(`**Thank you so much for joining our server!** Here's a couple of things you can do to get started:`)
+    .setDescription(`**Thank you so much for joining our server!** Here's a few things you can do to get started:`)
     .addFields([
       {
-        name: "Get Roles",
+        name: "Get some roles!",
         value: "Use the ``/roles`` command in any channel to get a list of **roles you can earn**!"
       },
       {
-        name: "Link Account",
-        value: "Send a message in any channel and use the ``/password`` command to **receive a personal code** to use in Duck Simulator 2!"
+        name: "Link your account!",
+        value: "Use the ``/password`` command to **receive a personal code** to use in Duck Simulator 2!"
       },
       {
-        name: "Level Up",
+        name: "Grind for a whole bunch of XP!",
         value: "Talk in the server to get a **higher position** on our [custom leaderboard](https://ducksimulator.com/leaderboard)!"
       },
       {
-        name: "Speedrun",
-        value: "Check the ``⌚speedrun-feed`` channel for the latest **speedrun times**!"
+        name: "See how fast people can speedrun the game!",
+        value: "Check the ``#⌚speedrun-feed`` channel for the latest **speedrun times**!"
       }
-    ]);
+    ])
+    .setFooter({ text: "Duck Simulator", iconURL: client.user.avatarURL() });
   member.send({ embeds: [embed] });
 }
 
